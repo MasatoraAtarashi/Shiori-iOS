b7e61764acdc20da99c3dbfa70609f0391bb9288
あたらしまさとら <masatora.atarashi@gmail.com>
2019-09-13 16:16:12 +0900

    Initial Commit

---
+++ Shiori/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shiori
+//
+//  Created by あたらしまさとら on 2019/09/13.
+//  Copyright © 2019 Masatora Atarashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
