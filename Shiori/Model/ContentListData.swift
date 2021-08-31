a214021397b8579add8359c6ae4072bb805404b6
masatora <atarashi.masatora@gmail.com>
2021-08-31 22:36:03 +0900

    コンテンツ一覧レスポンスをデコード

---
+++ Shiori/Model/ContentListData.swift
@@ -0,0 +1,50 @@
+//
+//  ContentListData.swift
+//  Shiori
+//
+//  Created by あたらしまさとら on 2021/08/31.
+//  Copyright © 2021 Masatora Atarashi. All rights reserved.
+//
+
+import Foundation
+
+struct ContentListData: Codable {
+    let meta: Meta
+    let data: Data
+}
+
+struct Meta: Codable {
+    let q: String?
+    let per_page: Int?
+    let page: Int?
+    let nextPage: Int?
+    let liked: Bool?
+}
+
+struct Data: Codable {
+    let content: [Content]
+}
+
+struct Content: Codable {
+    let id: Int
+    let content_type: String
+    let title: String
+    let url: String
+    let sharing_url: String
+    let file_url: String?
+    let thumbnail_img_url: String
+    let scroll_position_x: Int
+    let scroll_position_y: Int
+    let max_scroll_position_x: Int
+    let max_scroll_position_y: Int
+    let video_playback_position: Int
+    let specified_text: String?
+    let specified_dom_id: String?
+    let specified_dom_class: String?
+    let specified_dom_tag: String?
+    let liked: Bool?
+    let delete_flag: Bool
+    let deleted_at: String?
+    let created_at: String
+    let updated_at: String
+}
--- Shiori/Model/ContentManager.swift
+++ Shiori/Model/ContentManager.swift
@@ -10,9 +10,9 @@ import Foundation
 
 let const = Const()
 struct ContentManager {
-    
+
     func fetchContentList() {
-        let getContentListURL = "\(const.baseURL)/heartbeat"
+        let getContentListURL = "\(const.baseURL)/v1/content"
         performRequest(with: getContentListURL)
     }
 
@@ -24,35 +24,46 @@ struct ContentManager {
         print("performRequest")
         print("performRequest")
         if let url = URL(string: urlString) {
-            print("url ok")
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
+            var request = URLRequest(url: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            // TODO: ちゃんとした値を入れる
+            request.setValue("unko@gmail.com", forHTTPHeaderField: "uid")
+            request.setValue("BTDPytpbkqjPSzlHWmj0fg", forHTTPHeaderField: "client")
+            request.setValue("8sT9I1VQ5qAx_MkqUBLb2Q", forHTTPHeaderField: "access-token")
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
                 if error != nil {
                     // エラー処理を書く
                     print("Error", error)
                     return
                 }
                 if let safeData = data {
-                    //                    if let contentList = self.parseJSON(safeData) {
-                    //                        print(contentList)
-                    //                    }
-                    print(response)
+                    if let contentList = self.parseJSON(safeData) {
+                        for content in contentList.data.content {
+                            print(content)
+                        }
+                    }
                 }
-                print(data)
-                print(response)
             }
             task.resume()
         }
     }
 
-    //    func parseJSON(_ contentListData: Data) -> [ContentModel?] {
-    //        let decoder = JSONDecoder()
-    //        do {
-    //            let decodeData try de
-    //        } catch {
-    //            // エラー処理を書く
-    //            print("Error")
-    //            return nil
-    //        }
-    //    }
+    func parseJSON(_ contentListData: Foundation.Data) -> ContentListData? {
+        print("parseJSON")
+        print("parseJSON")
+        print("parseJSON")
+        print(String(data: contentListData, encoding: .utf8))
+
+        print("parseJSON")
+        print("parseJSON")
+        let decoder = JSONDecoder()
+        do {
+            let decodeData = try decoder.decode(ContentListData.self, from: contentListData)
+            return decodeData
+        } catch {
+            // エラー処理を書く
+            print("Error: ", error)
+            return nil
+        }
+    }
 }
