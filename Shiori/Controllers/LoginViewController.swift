3353a91294a3ba373c6fbb7939e68af954bb0662
masatora <atarashi.masatora@gmail.com>
2021-09-12 19:57:51 +0900

    ログイン画面を追加

---
+++ Shiori/Controllers/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Shiori
+//
+//  Created by あたらしまさとら on 2021/09/12.
+//  Copyright © 2021 Masatora Atarashi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
