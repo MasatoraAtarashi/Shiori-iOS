f8177a0f2298395cc1986c9c90b4ea15789ac75f
masatora <atarashi.masatora@gmail.com>
2021-09-14 01:45:38 +0900

    SignUpViewControllerを作成

---
+++ Shiori/Controllers/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewController.swift
+//  Shiori
+//
+//  Created by あたらしまさとら on 2021/09/14.
+//  Copyright © 2021 Masatora Atarashi. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+}
