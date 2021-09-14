4f58e9cf5e8ad7748621b5fc32c09ab9d3e6b874
masatora <atarashi.masatora@gmail.com>
2021-09-14 21:41:59 +0900

    style: 会員登録画面のUI改善

--- Shiori/Controllers/SignUpViewController.swift
+++ Shiori/Controllers/SignUpViewController.swift
@@ -11,4 +11,43 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailInputField: UITextField!
+    @IBOutlet weak var passwordInputField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signUpWithTwitterButton: UIButton!
+    @IBOutlet weak var signUpWithGithubButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+        initViewUI()
+    }
+
+    func initViewUI() {
+        // メールアドレス入力欄のUI
+        emailInputField.layer.borderWidth = 1
+        emailInputField.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        emailInputField.layer.cornerRadius = 5
+
+        // パスワード入力欄のUI
+        passwordInputField.layer.borderWidth = 1
+        passwordInputField.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        passwordInputField.layer.cornerRadius = 5
+
+        // 会員登録ボタンのUI
+        signUpButton.layer.cornerRadius = 5
+
+        // Twitter会員登録のUI
+        signUpWithTwitterButton.layer.borderWidth = 1
+        signUpWithTwitterButton.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        signUpWithTwitterButton.layer.cornerRadius = 5
+
+        // Github会員登録のUI
+        signUpWithGithubButton.layer.borderWidth = 1
+        signUpWithGithubButton.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        signUpWithGithubButton.layer.cornerRadius = 5
+    }
 }
