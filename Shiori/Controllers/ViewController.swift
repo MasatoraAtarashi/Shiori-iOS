953c426b66dd17dda70efa0b9df7dfe817e1868a
masatora <atarashi.masatora@gmail.com>
2021-09-20 17:54:06 +0900

    refactor: format

--- Shiori/Controllers/ViewController.swift
+++ Shiori/Controllers/ViewController.swift
@@ -690,21 +690,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     // NOTE: 危険なコードなので呼び出さない
-//    // ローカルストレージ内のすべてのコンテンツを削除
-//    func deleteAllRecords() {
-//        let delegate = UIApplication.shared.delegate as! AppDelegate
-//        let context = delegate.persistentContainer.viewContext
-//
-//        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Article")
-//        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
-//
-//        do {
-//            try context.execute(deleteRequest)
-//            try context.save()
-//        } catch {
-//            print("There was an error")
-//        }
-//    }
+    //    // ローカルストレージ内のすべてのコンテンツを削除
+    //    func deleteAllRecords() {
+    //        let delegate = UIApplication.shared.delegate as! AppDelegate
+    //        let context = delegate.persistentContainer.viewContext
+    //
+    //        let deleteFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Article")
+    //        let deleteRequest = NSBatchDeleteRequest(fetchRequest: deleteFetch)
+    //
+    //        do {
+    //            try context.execute(deleteRequest)
+    //            try context.save()
+    //        } catch {
+    //            print("There was an error")
+    //        }
+    //    }
 
     // MARK: Subscripts
 }
