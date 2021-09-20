5fe7ad17bc846f16537c4c211e68212ff365549c
masatora <atarashi.masatora@gmail.com>
2021-09-21 03:36:36 +0900

    サムネイル画像取得ロジックを改善

--- Share/GetData.js
+++ Share/GetData.js
@@ -54,15 +54,36 @@ MyPreprocessor.prototype = {
             url = url + "?t=" + h + "h" + m + "m" + s + "s"
         }
         
-        var pi = document.getElementsByTagName('meta');
-        var image = "";
-        for(i=0;i<pi.length;i++){
-            if(pi[i].getAttribute("property")=="og:image"){
-                 image = pi[i].getAttribute("content");
+        // サムネイル画像を取得
+//        var pi = document.getElementsByTagName('meta');
+//        var image = "";
+//        for(i=0;i<pi.length;i++){
+//            if(pi[i].getAttribute("property")=="og:image"){
+//                 image = pi[i].getAttribute("content");
+//            }
+//        }
+//        if(image == "") {
+//            image = document.images[0].src;
+//        }
+        var image = ""
+        if (url?.includes('https://m.youtube.com/')) {
+            // youtubeのvideo-idを取得
+            var videoId = url.split('v=')[1];
+            var ampersandPosition = videoId.indexOf('&');
+            // video-idの&=以降を削除
+            if (ampersandPosition != -1) {
+                videoId = videoId.substring(0, ampersandPosition);
             }
-        }
-        if(image == "") {
-            image = document.images[0].src;
+            image = "http://img.youtube.com/vi/" + videoId + "/0.jpg";
+        } else {
+            var largest = 0;
+            var largestImg;
+            Array.from(document.body.getElementsByTagName('img')).forEach(function(e) {
+                if (largest < e.height) {
+                    largestImg = e;largest = e.height
+                }
+            });
+            image = largestImg.src;
         }
 
         var dateString = ""
